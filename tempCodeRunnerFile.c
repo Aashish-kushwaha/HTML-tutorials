19f40540ef5a5c385ed06c81076f96f19b55b502
Aashish-kushwaha <kushwahaashu444@gmail.com>
2022-11-27 22:09:19 +0530

    fonts

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,36 @@
 #include<stdio.h>
-#include<unistd.h>
 int main()
 {
-    fork();
-    printf("hello\n");
-    return 0;
+    int a,b,c;
+    int min;
+    float avg;
+    printf("enter three number:");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a<b)
+    {
+        if(a<c)
+        {
+            min=a;
+        }
+        else
+        {
+            min=c;
+        }
+    }else
+    {
+        
+        if(b<c)
+        {
+            min=b;
+        }
+        else
+        {
+            min=c;
+        }
+
+    }
+    printf("minimum element:%d",c);
+    avg=((a+b+c)-min)/2.0;
+    printf("avg of best two:%f",avg);
 }
